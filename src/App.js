2f4f3926758d14b3f57266079f82e43d682397f1
sibashish726 <sibashish.biswas@lumen.com>
2022-09-27 09:56:21 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component }  from 'react';
+import './App.css'
 function App() {
+  const handlePrint = () =>{
+    window.print()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <header>
+          <h2>Invoice App</h2>
+          <div>
+            <ul>
+              <li><button onClick={handlePrint}>Print</button></li>
+              <li>Download</li>
+              <li>Send</li>
+            </ul>
+          </div>
+        </header>
+        
+        <section>
+            {/* <input type="text" name="text" id="text" placeholder="Enter your name" required /> */}
+            <h2>Sibashish Biswas</h2>
+            <p>Your address</p>
+        </section>
+
+        <section>
+          <h2>Client name</h2>
+          <p>Clients address</p>
+        </section>
+
+        <article>
+          <ul>
+            <li>Invoice Number</li>
+            <li>Invoice date</li>
+            <li>Due date</li>
+            <li></li>
+          </ul>
+        </article>
+      </main>
+    </>
   );
 }
 
